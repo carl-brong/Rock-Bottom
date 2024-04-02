ef2745be29c005e98556410f16dbeb2d6481e459
carl-brong <70544412+carl-brong@users.noreply.github.com>
2024-04-02 10:44:52 -0700

    The game is playable when built
    Bug todo list will be shared soon

--- Assets/Scripts/Misc/FallingPlatform.cs
+++ Assets/Scripts/Misc/FallingPlatform.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+//using UnityEditor.Rendering.PostProcessing;
 using UnityEngine;
 //Carl Brong 
 //Falling Platforms
 public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private float fallDelayTime;
-    [SerializeField] private float delayDestroy;
+    private float resetTime = 5f;
+    //[SerializeField] private float delayDestroy;
 
     private bool isFalling = false;
     [SerializeField] private Rigidbody2D rb;
+    Vector2 defaultPos;
+
+    private void Start()
+    {
+        defaultPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -22,12 +31,29 @@ public class FallingPlatform : MonoBehaviour
             StartCoroutine(Fall());
         }
     }
+    /*private void Update()
+    {
+        if (Restarter.RestartGame`)
+        {
+
+        }
+    }*/
     private IEnumerator Fall()
     {
         isFalling = true;
         yield return new WaitForSeconds(fallDelayTime);
-
+        
         rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, delayDestroy);
+
+        yield return new WaitForSeconds(resetTime);
+        //.Destroy(gameObject, delayDestroy);
+        Reset();
+        isFalling = false;
+    }
+
+    private void Reset()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        transform.position = defaultPos;
     }
 }
