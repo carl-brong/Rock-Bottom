da40f51e390a137237e4ef2727cce878a561848f
carl-brong <70544412+carl-brong@users.noreply.github.com>
2024-04-27 11:42:05 -0700

    Epilepsy scene
    still can't build

--- Assets/Scripts/PersistObject/PersistObject.cs
+++ Assets/Scripts/PersistObject/PersistObject.cs
@@ -12,15 +12,15 @@ public class PersistObject : MonoBehaviour
 
     private void Start()
     {
+         
         var objects = FindObjectsOfType<PersistObject>();
         foreach (var obj in objects)
         {
             if (obj == this) break;
-            if (obj.objectID == objectID)
-            {
-                Destroy(gameObject);
-            }
+            if (obj.objectID == objectID) Destroy(gameObject);
+  
         }
+        
         DontDestroyOnLoad(gameObject);
     }
 }
