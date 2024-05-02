62b1dcd4088084d800e110a7e066e174604a594c
vlee4293-school <vlee4293+school@gmail.com>
2024-05-02 10:11:36 -0700

    minor fixes to colliders

--- Assets/VDev/Game/CheckpointTrigger.cs
+++ Assets/VDev/Game/CheckpointTrigger.cs
@@ -10,18 +10,20 @@ public class CheckpointTrigger : MonoBehaviour
 {
     private Player _player;
     private bool _triggered;
+    private Collider2D collider;
     
     private void Awake()
     {
         _player = GameObject.FindWithTag("Player").GetComponent<Player>();
         _triggered = false;
+        collider = GetComponent<Collider2D>();
     }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !_triggered)
         {
-            _player.startpos = other.transform.position;
+            _player.startpos = new Vector3(transform.position.x + collider.offset.x, transform.position.y + collider.offset.y + 0.5f);
             _triggered = true;
             Debug.Log(_player.startpos);
         }
