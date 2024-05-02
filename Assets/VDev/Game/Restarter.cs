6a7208aa08a42746e8d2229b00abbe865567cdca
carl-brong <70544412+carl-brong@users.noreply.github.com>
2024-05-01 19:26:31 -0700

    Fixes

--- Assets/VDev/Game/Restarter.cs
+++ Assets/VDev/Game/Restarter.cs
@@ -7,20 +7,20 @@ public class Restarter : MonoBehaviour
 {
     private GameObject _playerObject;
     private Player _player;
-    private UIManagerGameplay restart;
+    
     
  
     public void RestartGame()
     {
-        //restart = GetComponent<UIManagerGameplay>();
+        
         _playerObject = GameObject.FindWithTag("Player");
         _player = _playerObject.GetComponent<Player>();
         _player.transform.position = _player.startpos;
         _player.Rb.velocity = Vector2.zero;
         _player.HealHealth(1000);
         _player.transform.position = _player.startpos;
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        //restart.CloseGameOverMenu(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
 
 
 
